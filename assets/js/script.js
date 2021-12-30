faf0f5822323063e8f245cd3f8d1e61e20fc2e3b
jirachaya-kiri <jirachaya.kiri@berkeley.edu>
2021-12-29 19:29:14 -0800

    3.0
    Completely redid portfolio. Pushing now bc VSCode is just showing a blank screen

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//  //name pronunciation appearing
+//  let leftArrow = document.getElementById("left-arrow");
+//  let rightArrow = document.getElementById("right-arrow")
+ 
+//  function pronunciationAppears() {
+//     leftArrow.classList.remove('hidden');
+//      leftArrow.style.animation = "left-arrow-rotate 1s ease 1s 1 normal";
+//      rightArrow.classList.remove('hidden');
+//      rightArrow.style.animation = "right-arrow-rotate 1s ease 1s 1 normal";
+//  }
+ 
+// window.onload = pronunciationAppears();
